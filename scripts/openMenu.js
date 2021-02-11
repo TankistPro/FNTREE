320e8835e95b7f6b54076da60b1b4c9475f08a11
TankistPro <nakol.nayk@mail.ru>
2021-02-11 22:34:15 +0300

    add navigation menu

--- scripts/openMenu.js
+++ scripts/openMenu.js
@@ -1,14 +1,15 @@
 const menuIcon = document.querySelector(".menu-icon");
 const menu = document.querySelector(".menu-burger-block");
 const close = document.querySelector(".close-menu");
+const wrapper = document.querySelector(".wrapper");
+const body = document.querySelector("body");
 
 const openMenu = () => {
-    menu.style.top = "0";
-}
-
-const closeMenu = () => {
-    menu.style.top = "-50%";
+    wrapper.style.transition = ".3s all";
+    wrapper.style.backgroundColor = "rgba(19, 0, 48, 0.33)";
+    wrapper.style.transform = "translateX(288px)";
+    body.style.overflow = "hidden";
+    menu.style.left= "0";
 }
 
 menuIcon.addEventListener("click", openMenu);
-close.addEventListener("click", closeMenu);
