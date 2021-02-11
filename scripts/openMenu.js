4081837468654f6b57b006ab8c96c44c4ad299b1
TankistPro <nakol.nayk@mail.ru>
2021-02-11 23:16:44 +0300

    add fix menu burger

--- scripts/openMenu.js
+++ scripts/openMenu.js
@@ -3,13 +3,26 @@ const menu = document.querySelector(".menu-burger-block");
 const close = document.querySelector(".close-menu");
 const wrapper = document.querySelector(".wrapper");
 const body = document.querySelector("body");
+const overlay = document.querySelector(".overlay");
 
 const openMenu = () => {
-    wrapper.style.transition = ".3s all";
-    wrapper.style.backgroundColor = "rgba(19, 0, 48, 0.33)";
-    wrapper.style.transform = "translateX(288px)";
-    body.style.overflow = "hidden";
-    menu.style.left= "0";
+    if(!menu.classList.contains("open")) {
+        menu.classList.add("open");
+        wrapper.style.transition = ".3s all";
+        wrapper.style.transform = "translateX(288px)";
+        body.style.overflow = "hidden";
+        overlay.style.display = "block";
+        menu.style.left= "0";
+    }
 }
 
+const closeMenu = () => {
+    console.log("click")
+    menu.style.left= "-100%";
+    wrapper.style.transform = "translateX(0px)";
+    menu.classList.remove("open");
+    overlay.style.display = "none";
+}
+
+overlay.addEventListener("click", closeMenu)
 menuIcon.addEventListener("click", openMenu);
