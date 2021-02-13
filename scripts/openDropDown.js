671b38f0a306f7a0ad08f3ad792765dc02d68bfe
TankistPro <nakol.nayk@mail.ru>
2021-02-13 20:16:23 +0300

    fix sign in hover

--- scripts/openDropDown.js
+++ scripts/openDropDown.js
@@ -3,7 +3,6 @@ const items = document.querySelectorAll(".dropdowm-block p");
 items.forEach( item => {
     item.addEventListener('click', () => {
         const dropDowm = item.closest('.dropdowm-block');
-        // console.log(dropDowm)
         if (!dropDowm.classList.contains('open')) {
             dropDowm.classList.add("open")
         } else {
