0cbc2491d346afe5c33a53ab2877ea9c37b7c289
AnnaStimp <58040522+AnnaStimp@users.noreply.github.com>
2021-02-14 23:19:34 +0300

    slider fp

--- scripts/fp-slider.js
+++ scripts/fp-slider.js
@@ -1,11 +1,11 @@
 const slider = document.getElementsByClassName('fortnite-packages__slider')[0];
 const right = slider.getElementsByClassName('fp-btn_slider')[0];
 const wrap = slider.getElementsByClassName('fortnite-packages__slider__blocks')[0];
-let items = wrap.children;
+let itemsWrap = wrap.children;
 
 wrap.style.transform = 'translateX(0)';
-for (let i = 0; i<items.length; i++) {
-    items[i].style.order = `${i}`;
+for (let i = 0; i<itemsWrap.length; i++) {
+    itemsWrap[i].style.order = `${i}`;
 }
 
 if (window.innerWidth > 1162) {
@@ -16,31 +16,46 @@ if (window.innerWidth > 1162) {
 
 
 function gorizontalSlider() {
-    for (let i=0; i<items.length; i++) {
-        let k = items[i].style.order;
-        if (k==0) {
-            k = Number(items.length);
+    let firstItem;
+    for (let i=0; i<itemsWrap.length; i++) {
+        if (itemsWrap[i].style.order == '0') {
+            firstItem = itemsWrap[i];
         }
-        k = Number(k)-1;
-        items[i].style.order = `${k}`;
     }
-    wrap.style.transition = 'none';
-    wrap.style.transform = `translateX(${33.3333}%)`;
-    
+    firstItem.style.transition = '.20s ease';
+    firstItem.style.opacity = '0';
+
+    setTimeout(() => {
+        firstItem.style.transition = 'none';
+        for (let i=0; i<itemsWrap.length; i++) {
+            let k = itemsWrap[i].style.order;
+            if (k==0) {
+                k = Number(itemsWrap.length);
+            }
+            k = Number(k)-1;
+            itemsWrap[i].style.order = `${k}`;
+        }
+        wrap.style.transition = 'none';
+        wrap.style.transform = `translateX(${33.3333}%)`;
+        firstItem.style.opacity = '1';
+    }, 100);
+    // wrap.style.transition = 'none';
+    // wrap.style.transform = `translateX(${33.3333}%)`;
+
     setTimeout(() => {
         wrap.style.transition = 'transform 0.6s ease';
         wrap.style.transform = `translateX(0%)`;
-    }, 50);
+    }, 200);
 }
 
 function verticalSlider() {
-    for (let i=0; i<items.length; i++) {
-        let k = items[i].style.order;
+    for (let i=0; i<itemsWrap.length; i++) {
+        let k = itemsWrap[i].style.order;
         if (k==0) {
-            k = Number(items.length);
+            k = Number(itemsWrap.length);
         }
         k = Number(k)-1;
-        items[i].style.order = `${k}`;
+        itemsWrap[i].style.order = `${k}`;
     }
     wrap.style.transition = 'none';
     wrap.style.transform = `translateY(${33.3333}%)`;
