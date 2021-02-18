bc197dcccedd49e1bd3da65371acf4bf6d4060be
AnnaStimp <58040522+AnnaStimp@users.noreply.github.com>
2021-02-18 21:41:54 +0300

    upgrade slider

--- scripts/up-table.js
+++ scripts/up-table.js
@@ -3,42 +3,40 @@ const tuples = document.querySelector('.tbl-content').children;
 const control_pages = controler.querySelector('.pages');
 
 let main_page = control_pages.querySelector('.active').textContent;
-
 let num_pages = Number(main_page)*8;
-
-function closeTuples(num_pages) {
-    for (let i = 0; i<tuples.length; i++) {
-        if (i>num_pages-9 && i<num_pages) {
-            tuples[i].style.display = 'table-row';
-            continue
-        }
-        tuples[i].style.display = 'none';
-    }
-}
+closeTuples(num_pages);
 
 for (let i=0; i<control_pages.children.length; i++) {
-    control_pages.children[i].addEventListener('click', (event) => {
-        for (let i=0; i<control_pages.children.length; i++) {
-            control_pages.children[i].classList.remove('active');
-        }
-        event.target.classList.add('active');
-
-        let num_pages = Number(event.target.textContent)*8;
-        closeTuples(num_pages);
-    })
+    control_pages.children[i].addEventListener('click', () => changePage(event));
 }
 
 window.addEventListener('resize', () => {
+    console.log('h');
     if (window.innerWidth <= 1100) {
-        for (let i = 0; i<tuples.length; i++) {
-            if (i>num_pages-9 && i<num_pages) {
-                continue
-            }
-            tuples[i].style.display = 'none';
-        }
+        closeTuples(num_pages);
     } else {
         for (let i = 0; i<tuples.length; i++) {
             tuples[i].style.display = 'table-row';
         }
     }
 })
+
+function changePage(event) {
+    for (let i=0; i<control_pages.children.length; i++) {
+        control_pages.children[i].classList.remove('active');
+    }
+    event.target.classList.add('active');
+
+    num_pages = Number(event.target.textContent)*8;
+    closeTuples(num_pages);
+}
+
+function closeTuples(el) {
+    for (let i = 0; i<tuples.length; i++) {
+        if (i>el-9 && i<el) {
+            tuples[i].style.display = 'table-row';
+            continue
+        }
+        tuples[i].style.display = 'none';
+    }
+}
